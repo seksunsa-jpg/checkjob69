47c6f58406880ba5127b9e10f1500963dfa41fdf
seksunsa-jpg <seksun.sa@udru.ac.th>
2026-09-24 14:57:00 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="grad-audit69-v2";   // แยกจากระบบหลักสูตรเดิม (grad-audit-*)
+const CACHE="grad-audit69-v3";   // แยกจากระบบหลักสูตรเดิม (grad-audit-*)
 const FILES=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
